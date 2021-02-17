65ec9b0179a452027817caf7d3888940d725ed50
Lennart Luttkus <lennart.luttkus@uni-a.de>
2021-02-17 16:31:04 +0100

    connect local workspace to docker container

--- docker_run.sh
+++ docker_run.sh
@@ -3,4 +3,9 @@
 docker run --gpus all -dit --name gym_docker \
         -p 5900:5900 \
         -p 8888:8888 \
+        -v $(pwd)/workspace:/root/workspace \
         gym_docker:v1
+
+sleep 5
+
+docker logs gym_docker
